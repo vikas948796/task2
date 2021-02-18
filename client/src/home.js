48cc5c350739e101f724e7d1d8729c412442afc5
vikas948796 <vikas948796@gmail.com>
2021-02-18 09:48:22 +0530

    first commit

--- client/src/home.js
+++ client/src/home.js
@@ -1,10 +1,7 @@
 import React  from 'react'
 import  { useHistory } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import{FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {  faEnvelope, faKey, faPhone, faUser } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
-import logo from './a1.jpg';
 import logo1 from './a2.jpg';
 import img1 from './b1.PNG';import img2 from './c1.PNG';import img3 from './d1.PNG';
 import img4 from './e1.PNG';import img5 from './f1.PNG';import img6 from './g1.PNG';
@@ -80,10 +77,64 @@ useEffect(() => {
 
   return (
     <div className="App"> 
-    <img src={logo} id="er"></img>
+    
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark" style={{zIndex:'2'}}>
+  <a class="navbar-brand" style={{color:"White",fontSize:"30px"}} href="#">ne<span style={{color:"red",fontSize:"30px"}}>a</span>r</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+        <a class="nav-link " href="#"></a>
+      </li>
+      
+      <li class="nav-item" >
+        <a class="nav-link" href="#">Covid-19</a>
+      </li>
+
+
+      <li class="nav-item  dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Solution</a> 
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}>Near for<br /><b>Publishers</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}>Near for<br /><b>Marketers</b></span></a>
+      </div>
+      </li>
+
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Products</a>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}>Allspark<br /><b>AI-powered Audience Curation</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}>CARBON<sup>TM</sup><br /><b>Real-World Data Enrichment</b></span></a>
+      </div>
+      </li>    
+      <li class="nav-item">
+        <a class="nav-link" href="#">The Data</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">About Us</a>
+        <div class="dropdown-menu">
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Leadership</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Investors</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Board</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Resources</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Blog</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Newsroom</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Careers</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Privacy</b></span></a>
+        <a class="dropdown-item" href="#"><span style={{fontSize:"14.4px"}}><b>Contact Us</b></span></a>
+      </div>
+      </li>
+    </ul>
+  </div>  
+</nav>
+
+
     <div className="header"><div class="centered"><span style={{fontSize:"3vw"}}>Explore and do what you love</span><br />
     <center><span id="span1">Welcome to Near Careers</span><br /><br /><button type="button" class="btn btn-primary" id="btn111">View Openings</button></center></div>
-    <img src={logo1} id="er1"></img>
+    <img src={logo1} style={{position: "absolute",display: "block"}} id="er1"></img>
     </div>                         
     <br />             
    <center> <div class="btn-group">
@@ -167,49 +218,181 @@ useEffect(() => {
 </div>
 <br /><br />
     <img src={i7} id="er5"></img>
-    <span id="span2" style={{marginLeft:'40%',fontSize:'4vw'}}>#Welcome to Near Careers</span>
-    <center><span id="span3">Open Positions</span></center>
+  
+      <center><span id="span3">Open Positions</span></center>
   <span id="span4">Engineering</span>
   
-<table>
-  <tr><td>  <div class="container">
-  <div class="card" style={{width:"400px"}}>
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
     <div class="card-body">
-     <h3 class="card-title">Data Engineer</h3> <hr></hr>
-      <p class="card-text">bangalore</p>
+     <h4 class="card-title" id="card_title"><b>Data Engineer</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
       <a href="#" class="btn btn-primary">See Profile</a>
     </div>
   </div></div>
-  </td>
-  <td>  <div class="container">
-  <div class="card" style={{width:"400px"}}>
+  
+  </div>
+  <div class="col-md-4">  
+  <div class="container" id="left2" class="card2">
+  <div class="card" style={{width:"98%"}}>
     <div class="card-body">
-     <h3 class="card-title">Lead, Data Engineer</h3> <hr></hr>
-      <p class="card-text">bangalore</p>
+     <h4 class="card-title" id="card_title"><b>Senior Engineer, Platform Engineering</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
       <a href="#" class="btn btn-primary">See Profile</a>
     </div>
   </div></div>
-  </td>
-  <td>  <div class="container">
-  <div class="card" style={{width:"400px"}}>
+  </div>
+</div>
+<span id="span4">Product</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
     <div class="card-body">
-     <h3 class="card-title">Senior Engineer, Platform Engineering</h3> <hr></hr>
-      <p class="card-text">bangalore</p>
+     <h4 class="card-title" id="card_title"><b>VP - Carbon</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
       <a href="#" class="btn btn-primary">See Profile</a>
     </div>
   </div></div>
-  </td>
-  </tr>
-</table>
- 
+  </div>
+  <div class="col-md-4">  
+  <div class="container" id="left2" class="card2">
+  <div class="card" style={{width:"98%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Vice President of Data Partnerships</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div>
+  <div class="col-md-4">  
+  <div class="container" id="left2" class="card2">
+  <div class="card" style={{width:"98%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Product Manager</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div>  
+</div>
+<span id="span4">Data Science</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Senior, Data Science</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div></div>
 
- 
+  <span id="span4">Research & Analytics</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Research Associate</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div></div>
 
 
+  <span id="span4">Marketing</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Digital Marketing Technologist</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div>
+  <div class="col-md-4">  
+  <div class="container" id="left2" class="card2">
+  <div class="card" style={{width:"98%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Content Marketing Specialist</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div>
+  <div class="col-md-4">  
+  <div class="container" id="left2" class="card2">
+  <div class="card" style={{width:"98%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Product Manager</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div>  
+</div>
+<div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>VP - CARBON</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div></div>
+
+  <span id="span4">Platform Solutions</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Platform Solution Specialist</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div></div>
+  </div></div>
 
 
+  <span id="span4">Sales</span>
+  
+  <div class="row">
+  <div class="col-sm-4" class="col-md-4"   >     
+  
+  <div class="container" id="left1" class="card2">
+  <div class="card" style={{width:"99%"}}>
+    <div class="card-body">
+     <h4 class="card-title" id="card_title"><b>Strategic Sales Lead</b></h4> 
+      <p class="card-text">bangalore</p><hr></hr>
+      <a href="#" class="btn btn-primary">See Profil</a>
+    </div>
+  </div></div>
+  </div></div>
 
-    </div>    
+
+ </div>    
          )   
 }
 
